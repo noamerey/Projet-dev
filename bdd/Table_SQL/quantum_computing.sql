0f2dc42ca25427727d9d4d8d93b2f084996711cf
ValentinCl <122225576+ValentinCl@users.noreply.github.com>
2023-11-26 17:47:48 +0100

    Add files via upload

---
+++ bdd/Table_SQL/quantum_computing.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 26, 2023 at 04:46 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tutos_videos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quantum computing`
+--
+
+CREATE TABLE `quantum computing` (
+  `id` int(255) NOT NULL,
+  `titre` varchar(500) NOT NULL,
+  `auteur` varchar(100) NOT NULL,
+  `mots_cles` varchar(3000) NOT NULL,
+  `mp4` varchar(5000) NOT NULL,
+  `pdf` varchar(5000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `quantum computing`
+--
+
+INSERT INTO `quantum computing` (`id`, `titre`, `auteur`, `mots_cles`, `mp4`, `pdf`) VALUES
+(1, 'Deutsch-Jozsa algorithm explained', 'Adem RAHAL', '', 'https://www.youtube.com/playlist?list=PLOFEBzvs-VvrXTMy5Y2IqmSaUjfnhvBHR\r\n', ''),
+(2, 'From Bits To Qubits', 'Adem RAHAL', 'https://www.youtube.com/playlist?list=PLOFEBzvs-VvrXTMy5Y2IqmSaUjfnhvBHR', '', ''),
+(3, 'Introduction To Quantum Computing And Complexity', 'Adem RAHAL', '', 'https://www.youtube.com/playlist?list=PLOFEBzvs-VvrXTMy5Y2IqmSaUjfnhvBHR', ''),
+(4, 'Grover\'s Algorithm Explained', 'Adem RAHAL', '', 'https://www.youtube.com/playlist?list=PLOFEBzvs-VvrXTMy5Y2IqmSaUjfnhvBHR', ''),
+(5, 'Introduction to quantum machine learning', 'Adem RAHAL', '', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `quantum computing`
+--
+ALTER TABLE `quantum computing`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
