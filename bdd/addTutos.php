aacbb70da398d4d05154dd008f6a73cd0b5510b8
ValentinCl <122225576+ValentinCl@users.noreply.github.com>
2023-12-11 11:02:03 +0100

    Update addTutos.php

--- bdd/addTutos.php
+++ bdd/addTutos.php
@@ -1,1 +1,34 @@
+<?php
+
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $titre = $_POST["titre"];
+    $auteur = $_POST["auteur"];
+    $mots_cles = $_POST["mots_cles"];
+    $mp4 = $_POST["mp4"];
+    $pdf = $_POST["pdf"];
+
+    $sql = "INSERT INTO your_table_name (titre, auteur, mots_cles, mp4, pdf)
+            VALUES ('$titre', '$auteur', '$mots_cles', '$mp4', '$pdf')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Tutorial added successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+
+?>
 
